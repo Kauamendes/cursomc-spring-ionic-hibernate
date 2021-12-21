b6faf3ee7cb859d6ba593c7995d07e9c00b24412
Kauã Mendes <kauamendes80@hotmail.com>
2021-12-21 11:07:38 -0300

    criação do projeto

---
+++ src/main/java/com/kauamendes/projetocurso/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.kauamendes.projetocurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
