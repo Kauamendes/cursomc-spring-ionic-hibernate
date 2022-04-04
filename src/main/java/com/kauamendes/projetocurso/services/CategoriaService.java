2289746cf81689331e77612a8aaa0a39c9466700
Kauã Mendes <kauamendes80@hotmail.com>
2022-04-04 16:33:25 -0300

    Deletando uma Categoria com delete

--- src/main/java/com/kauamendes/projetocurso/services/CategoriaService.java
+++ src/main/java/com/kauamendes/projetocurso/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.kauamendes.projetocurso.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.kauamendes.projetocurso.domain.Categoria;
 import com.kauamendes.projetocurso.repositories.CategoriaRepository;
+import com.kauamendes.projetocurso.services.exceptions.DateIntegrityException;
 
 @Service
 public class CategoriaService {
@@ -28,6 +30,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DateIntegrityException("Não é ossivel excluir uma categoria que possui produtos");
+		}
 	}
 	
 }
