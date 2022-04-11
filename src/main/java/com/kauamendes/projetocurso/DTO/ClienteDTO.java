5ba1312d3cfe56389953fd19f345f5ad8c8a856d
Kauã Mendes <kauamendes80@hotmail.com>
2022-04-11 10:23:22 -0300

    Validacao customizada: email nao repetido na atualizacao do cliente

--- src/main/java/com/kauamendes/projetocurso/DTO/ClienteDTO.java
+++ src/main/java/com/kauamendes/projetocurso/DTO/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.kauamendes.projetocurso.domain.Cliente;
+import com.kauamendes.projetocurso.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
