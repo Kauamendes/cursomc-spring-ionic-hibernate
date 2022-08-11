fd99764272402b946cd71328738a64c9a77f089a
Kauã Mendes <kauamendes80@hotmail.com>
2022-08-11 14:09:21 -0300

    MockEmailService com Logger, Padrões Strategy e Template

---
+++ src/main/java/com/kauamendes/projetocurso/config/TestConfig.java
@@ -0,0 +1,26 @@
+package com.kauamendes.projetocurso.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
+	}
+	
+}
