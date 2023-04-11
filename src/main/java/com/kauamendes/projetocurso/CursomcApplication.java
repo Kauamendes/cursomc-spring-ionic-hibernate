8630f0e9c12b56f2126444c0f26a6eed1f64bd01
Kauã Mendes <kauamendes80@hotmail.com>
2023-04-11 15:29:50 -0300

    Autorizando endpoints para perfis especificos

--- src/main/java/com/kauamendes/projetocurso/CursomcApplication.java
+++ src/main/java/com/kauamendes/projetocurso/CursomcApplication.java
@@ -136,7 +136,7 @@ public class CursomcApplication implements CommandLineRunner{
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3,c4));
 		
 		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","36378912377",TipoCliente.PESSOAFISICA, be.encode("kaua123"));
-		cli1.addPerfil(Perfil.ADMIN);;
+		cli1.addPerfil(Perfil.ADMIN);
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
 		
 		Endereco e1 = new Endereco(null,"Rua Flores","300","Apto 303","Jardim","38220834",cli1,c1);
--- src/main/java/com/kauamendes/projetocurso/config/SecurityConfig.java
+++ src/main/java/com/kauamendes/projetocurso/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import com.kauamendes.projetocurso.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -42,8 +44,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String[] PUBLIC_MATHERS_GET = {
 			"/produtos/**",
-			"/clientes/**",
 			"/categorias/**"
+	};
+	
+	private static final String[] PUBLIC_MATHERS_POST = {
+			"/clientes/**",
 	};
 	
 	@Override
@@ -56,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, PUBLIC_MATHERS_GET).permitAll()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATHERS_POST).permitAll()
 			.antMatchers(PUBLIC_MATHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
